5012a8d4f78af1d5e97d19d4a8e74bf73386232b
Edwin <evill096@fiu.edu>
2022-09-19 17:14:23 -0400

    5:14 pm

--- react-website/src/components/Navbar.js
+++ react-website/src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom';
 import { Button } from './Button';
 import './Navbar.css';
@@ -19,12 +19,16 @@ function Navbar() {
   }
 };
 
+useEffect(()=>{
+  showButton();
+},[]);
+
 window.addEventListener('resize', showButton)
   return (
     <>
     <nav className="navbar">
         <div className="navbar-container">
-            <Link to="/" className="navbar-logo">
+            <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
                 TRAVL <i class="fa fa-podcast" aria-hidden="true"></i>
             </Link>
             <div className='menu-icon' onClick={handleClick}>
